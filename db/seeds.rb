e7726aa6e80c8acf93dad67c0d1ac191c7eaf9eb
Jordan Hudgens <jordan@devcamp.com>
2017-06-19 22:52:49 +0000

    Created seeds for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "
+    Now that there is the Tec-9, a crappy spray gun from South Miami. 
+    This gun is advertised as the most popular gun in American crime. Do you believe that shit? 
+    It actually says that in the little book that comes
+    with it: the most popular gun in American crime. Like they're actually proud of that shit. "
+    )
+end
+
+puts "10 Blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized:15
+  )
+end 
+
+puts "5 skill posts created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio title: #{portfolio_item}",
+    subtitle: "my service",
+    body: "You think water moves fast? You should see ice. It moves like it has a mind.",
+    main_image: "https://placekitten.com/g/600/400" ,
+    thumb_image: "https://placekitten.com/g/350/200" 
+    )
+end
+
+puts "9 portfolio pieces created"
